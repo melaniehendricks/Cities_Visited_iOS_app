d58a666f97cd9589e7a4d70b9fed565dfdb26659
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2020-03-20 17:00:46 -0700

    Initial Commit

---
+++ Lab5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab5
+//
+//  Created by Melanie Hendricks on 3/20/20.
+//  Copyright © 2020 Melanie Hendricks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
